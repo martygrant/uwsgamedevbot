f78b0cb3f6373f0063ac141e9e6f06cf4ed35edc
Tilman Raendchen <chopper98@hotmail.de>
2018-08-01 12:51:43 +0200

    Move and categorise code

--- main.py
+++ main.py
@@ -21,6 +21,8 @@ import discord
 from discord.ext import commands
 from weather import Weather, Unit
 
+import utilities as utils
+
 REPOSITORY_URL = "https://github.com/martygrant/uwsgamedevbot"
 VERSION_NUMBER = os.getenv('version')
 BOT_TOKEN = os.getenv('token')
@@ -40,16 +42,87 @@ class CustomBot(commands.Bot):
             return super().send_message(self.get_channel(destination), content, tts=tts, embed=embed)
         return super().send_message(destination, content, tts=tts, embed=embed)
 
+class Poll:
+    """An instance of this class handles the creation of a poll and monitoring of reactions"""
+
+    @property
+    def time_left(self):
+        """Gets the time left for this instance of the poll (in seconds)"""
+        return (timestamp() / 1000) - self.time_to_stop
+
+    @property
+    def embed(self):
+        """Constructs a Rich Embed for this poll"""
+        e = discord.Embed(type="rich",
+                          description="This is {}'s poll.".format(self.initiator.mention),
+                          timestamp=datetime.now() + timedelta(seconds=self.time_to_stop),
+                          colour=utils.generate_random_colour())
+
+        e.add_field(name="\u200b", value="**Options**", inline=False)
+        for i, option in enumerate(self.options):
+            current_votes = len(self.results[utils.ALPHABET[i]])
+            e.add_field(name="{}. {}".format(utils.ALPHABET[i].upper(), option), value="{} votes".format(current_votes), inline=True)
+
+        e.set_author(name=self.question,
+                     icon_url=self.initiator.avatar_url)
+        if self.time_left > 0:
+            e.set_footer(text="Time remaining: {} seconds".format(self.time_left))
+
+        return e
+
+    def __init__(self, question, options, duration, owner, message=None):
+        self.question = question
+        self.question_message = None
+        self.options = options
+        self.duration = duration
+        self.time_to_stop = (timestamp() / 1000) + duration
+        self.initiator = owner
+        self.results = {}
+        for i in range(len(self.options)):
+            self.results[utils.ALPHABET[i]] = []
+
+        # If a 'message' object already exists, use that
+        if isinstance(message, discord.Message):
+            self.channel = message.channel
+            self.question_message = message
+
+        # If not, create a new message in 'Poll.start()'
+        elif isinstance(message, discord.Channel):
+            self.channel = message
+        else:
+            raise TypeError("The \{message\} argument must be of type 'Discord.Message' or 'Discord.Channel'")
+
+    async def start(self):
+        """Starts the poll and creates a task for the poll to end using by the duration"""
+        # Send a new message if one wasn't already passed to the constructor
+        if self.question_message is None:
+            self.question_message = await BOT.send_message(self.channel, embed=self.embed)
+        BOT.ongoing_polls[self.question_message.id] = self
+
+        await self.add_reactions()
+        BOT.loop.create_task(self.stop())
+
+    async def stop(self):
+        """Stops the poll and posts the results"""
+
+        await BOT.wait_until_ready()
+        await asyncio.sleep(self.duration)
+
+        # TODO: finalise data to prettify output
+
+        await BOT.send_message(self.channel, "**{}**'s poll has finished. Here are the results.".format(self.initiator.name), embed=self.embed)
+        BOT.ongoing_polls.pop(self.question_message.id, self)
+
+    async def add_reactions(self):
+        """Adds the respective reactions for each option for users to react to"""
+        for i in range(len(self.options)):
+            await BOT.add_reaction(self.question_message, utils.resolve_emoji_from_alphabet(utils.ALPHABET[i].lower()))
+
+##### [ BOT INSTANTIATION ] #####
+
 BOT = CustomBot(description="Below is a listing for Bjarne's commands. Use '!' infront of any of them to execute a command, like '!help'", command_prefix="!")
 
-def refresh_config():
-    """Refreshes the config file and its properties"""
-    config_file = open('config.json', 'r')
-    BOT.config = config_file.read()
-    config_file.close()
-
-    decoder = json.JSONDecoder()
-    BOT.config = decoder.decode(BOT.config)
+##### [ EVENT LISTENERS ] #####
 
 @BOT.event
 async def on_ready():
@@ -61,7 +134,7 @@ async def on_member_join(member):
     """The 'on_member_join' event"""
 
     # Refresh channel IDs
-    refresh_config()
+    utils.refresh_config()
 
     welcome_message = """Welcome to the UWS Game Dev Society!
 
@@ -80,7 +153,7 @@ async def on_member_remove(member):
     """The 'on_member_remove' event"""
 
     # Refresh channel IDs
-    refresh_config()
+    utils.refresh_config()
 
     await BOT.send_message(BOT.config["channels"]["lobby"], "User **{}** has left the server. Goodbye!".format(str(member)))
 
@@ -92,9 +165,9 @@ async def on_reaction_add(reaction, user):
 
     current_poll = BOT.ongoing_polls[reaction.message.id]
     # If the reaction is the same as any of the existing reactions
-    if any(resolve_emoji_from_alphabet(option) == reaction.emoji for option in current_poll.results.keys()):
+    if any(utils.resolve_emoji_from_alphabet(option) == reaction.emoji for option in current_poll.results.keys()):
         # Add the user to the respective result object
-        selected_option = resolve_letter_from_emoji(reaction.emoji)
+        selected_option = utils.resolve_letter_from_emoji(reaction.emoji)
         if user.id not in current_poll.results[selected_option]:
             current_poll.results[selected_option].append(user.id)
 
@@ -109,14 +182,16 @@ async def on_reaction_remove(reaction, user):
 
     current_poll = BOT.ongoing_polls[reaction.message.id]
     # If the reaction is the same as any of the existing reactions
-    if any(resolve_emoji_from_alphabet(option) == reaction.emoji for option in current_poll.results.keys()):
+    if any(utils.resolve_emoji_from_alphabet(option) == reaction.emoji for option in current_poll.results.keys()):
         # Remove the user from the respective result object
-        deselected_option = resolve_letter_from_emoji(reaction.emoji)
+        deselected_option = utils.resolve_letter_from_emoji(reaction.emoji)
         if user.id in current_poll.results[deselected_option]:
             current_poll.results[deselected_option].remove(user.id)
 
         # Update the original poll message
         await BOT.edit_message(current_poll.question_message, embed=current_poll.embed)
+
+##### [ BOT COMMANDS ] #####
 
 @BOT.command()
 async def say(*something):
@@ -251,16 +326,6 @@ async def quote(ctx, *arg):
     random_message = messages[rand.randint(0, len(messages))]
     await BOT.say("{} once said: `{}`".format(user, random_message))
 
-ALPHABET = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
-            'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-def resolve_emoji_from_alphabet(letter):
-    """Returns the emoji representation of a letter"""
-    return chr(ord(letter) + 127365)
-
-def resolve_letter_from_emoji(emoji):
-    """Returns the character representation of a letter emoji (in lowercase)"""
-    return chr(ord(emoji) - 127365)
-
 @BOT.command(pass_context=True)
 async def poll(ctx):
     """Starts a new poll. Usage: !poll -Question -durationInSeconds -Option -Option -Option..."""
@@ -286,90 +351,6 @@ async def poll(ctx):
     new_poll = Poll(question, options, duration_float, ctx.message.author, ctx.message.channel)
     await new_poll.start()
 
-def generate_random_colour():
-    """Generates a random colour decimal"""
-    letters = "0123456789ABCDEF"
-    colour_string = ""
-    for i in range(6):
-        colour_string += rand.choice(letters)
-    return int(colour_string, 16)
-
-class Poll:
-    """An instance of this class handles the creation of a poll and monitoring of reactions"""
-
-    @property
-    def time_left(self):
-        """Gets the time left for this instance of the poll (in seconds)"""
-        return (timestamp() / 1000) - self.time_to_stop
-
-    @property
-    def embed(self):
-        """Constructs a Rich Embed for this poll"""
-        e = discord.Embed(type="rich",
-                          description="This is {}'s poll.".format(self.initiator.mention),
-                          timestamp=datetime.now() + timedelta(seconds=self.time_to_stop),
-                          colour=generate_random_colour())
-
-        e.add_field(name="\u200b", value="**Options**", inline=False)
-        for i, option in enumerate(self.options):
-            current_votes = len(self.results[ALPHABET[i]])
-            e.add_field(name="{}. {}".format(ALPHABET[i].upper(), option), value="{} votes".format(current_votes), inline=True)
-
-        e.set_author(name=self.question,
-                     icon_url=self.initiator.avatar_url)
-        if self.time_left > 0:
-            e.set_footer(text="Time remaining: {} seconds".format(self.time_left))
-
-        return e
-
-    def __init__(self, question, options, duration, owner, message=None):
-        self.question = question
-        self.question_message = None
-        self.options = options
-        self.duration = duration
-        self.time_to_stop = (timestamp() / 1000) + duration
-        self.initiator = owner
-        self.results = {}
-        for i in range(len(self.options)):
-            self.results[ALPHABET[i]] = []
-
-        # If a 'message' object already exists, use that
-        if isinstance(message, discord.Message):
-            self.channel = message.channel
-            self.question_message = message
-
-        # If not, create a new message in 'Poll.start()'
-        elif isinstance(message, discord.Channel):
-            self.channel = message
-        else:
-            raise TypeError("The \{message\} argument must be of type 'Discord.Message' or 'Discord.Channel'")
-
-    async def start(self):
-        """Starts the poll and creates a task for the poll to end using by the duration"""
-        # Send a new message if one wasn't already passed to the constructor
-        if self.question_message is None:
-            self.question_message = await BOT.send_message(self.channel, embed=self.embed)
-        BOT.ongoing_polls[self.question_message.id] = self
-
-        await self.add_reactions()
-        BOT.loop.create_task(self.stop())
-
-    async def stop(self):
-        """Stops the poll and posts the results"""
-
-        await BOT.wait_until_ready()
-        await asyncio.sleep(self.duration)
-
-        # TODO: finalise data to prettify output
-
-        await BOT.send_message(self.channel, "**{}**'s poll has finished. Here are the results.".format(self.initiator.name), embed=self.embed)
-        BOT.ongoing_polls.pop(self.question_message.id, self)
-
-    async def add_reactions(self):
-        """Adds the respective reactions for each option for users to react to"""
-        for i in range(len(self.options)):
-            await BOT.add_reaction(self.question_message, resolve_emoji_from_alphabet(ALPHABET[i].lower()))
-
 @BOT.command()
 async def weather(*arg):
     """Get current weather conditions at a specified location from Yahoo. E.g '!weather glasgow'"""
@@ -384,7 +365,7 @@ async def weather(*arg):
 
     location = weather_object.lookup_by_location(city)
 
-    embed = discord.Embed(type="rich", colour=generate_random_colour(), timestamp=datetime.now())
+    embed = discord.Embed(type="rich", colour=utils.generate_random_colour(), timestamp=datetime.now())
     embed.set_author(name=location.title)
     embed.add_field(name="Temperature", value="{}{}{}".format(location.condition.temp, degree_sign, location.units.temperature))
     embed.add_field(name="Condition", value=location.condition.text)
@@ -408,7 +389,7 @@ async def forecast(*arg):
     location = weather_object.lookup_by_location(city)
     forecasts = location.forecast
     count = 0
-    embed = discord.Embed(type="rich", colour=generate_random_colour(), timestamp=datetime.now())
+    embed = discord.Embed(type="rich", colour=utils.generate_random_colour(), timestamp=datetime.now())
     embed.set_author(name="5-day forecast for {}".format(location.title))
 
     for cast in forecasts:
@@ -418,5 +399,7 @@ async def forecast(*arg):
         embed.add_field(name=cast.date, value="{}\nHigh: {}{}{}\nLow: {}{}{}".format(cast.text, cast.high, degree_sign, location.units.temperature, cast.low, degree_sign, location.units.temperature))
 
     await BOT.say(embed=embed)
+
+##### [ BOT LOGIN ] #####
 
 BOT.run(BOT_TOKEN)
---
+++ utilities.py
@@ -0,0 +1,28 @@
+"""This file contains some utility functions"""
+
+ALPHABET = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
+            'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+def resolve_emoji_from_alphabet(letter):
+    """Returns the emoji representation of a letter"""
+    return chr(ord(letter) + 127365)
+
+def resolve_letter_from_emoji(emoji):
+    """Returns the character representation of a letter emoji (in lowercase)"""
+    return chr(ord(emoji) - 127365)
+
+def generate_random_colour():
+    """Generates a random colour decimal"""
+    letters = "0123456789ABCDEF"
+    colour_string = ""
+    for i in range(6):
+        colour_string += rand.choice(letters)
+    return int(colour_string, 16)
+
+def refresh_config():
+    """Refreshes the config file and its properties"""
+    config_file = open('config.json', 'r')
+    BOT.config = config_file.read()
+    config_file.close()
+
+    decoder = json.JSONDecoder()
+    BOT.config = decoder.decode(BOT.config)
