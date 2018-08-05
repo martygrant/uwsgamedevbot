fe50af471f04b0c1ce09d44b536bce0b296b702b
Tilman Raendchen <chopper98@hotmail.de>
2018-08-05 17:27:05 +0200

    Import random

--- utilities.py
+++ utilities.py
@@ -1,5 +1,7 @@
 """This file contains some utility functions"""
 
+import random as rand
+
 ALPHABET = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
             'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 def resolve_emoji_from_alphabet(letter):
