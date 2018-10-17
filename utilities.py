806364e9394cc3f27734a9202a4b9109481db0ad
Tilman Raendchen <chopper98@hotmail.de>
2018-10-17 22:32:31 +0100

    Add embed colours for easy reference

--- utilities.py
+++ utilities.py
@@ -1,6 +1,9 @@
 """This file contains some utility functions"""
 import random as rand
 
+COLOUR_SUCCESS = 0x4BB543
+COLOUR_ERROR = 0xB33A3A
+
 ALPHABET = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
             'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 def resolve_emoji_from_alphabet(letter):
